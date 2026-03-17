d70cebe172714c152b79a2889681cd77120f8f9f
vl4d_ok <vladqwe111@yandex.ru>
2026-03-17 19:09:10 +0500

    3 task

--- src/index.js
+++ src/index.js
@@ -29,8 +29,14 @@ function getCreatureDescription(card) {
 
 
 
+class Creature extends Card {
+    getDescriptions() {
+        return [getCreatureDescription(this), ...super.getDescriptions()];
+    }
+}
+
 // Основа для утки.
-class Duck extends Card {
+class Duck extends Creature {
     constructor(name = 'Мирная утка', maxPower = 2) {
         super(name, maxPower);
     }
@@ -40,7 +46,7 @@ class Duck extends Card {
 
 
 // Основа для собаки.
-class Dog extends Card {
+class Dog extends Creature {
     constructor(name = 'Пес-бандит', maxPower = 3) {
         super(name, maxPower);
     }
